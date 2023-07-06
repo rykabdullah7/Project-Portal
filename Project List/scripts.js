23ab2cf4b2c10056e81e27600e72bdb854085639
rykabdullah7 <75038215+rykabdullah7@users.noreply.github.com>
2023-07-06 15:30:13 +0500

    Update scripts.js

--- Project List/scripts.js
+++ Project List/scripts.js
@@ -1,4 +1,4 @@
-const hide_buttons = document.getElementsByClassName("btn");
+const hide_buttons = document.getElementsByClassName("hid");
 Array.from(hide_buttons).forEach((button) => {
     button.addEventListener('click',() => {
         const element = button.parentNode;
@@ -11,5 +11,13 @@ unhid.addEventListener("click", () => {
     Array.from(hide_buttons).forEach((button) =>{
         const element = button.parentNode;
         element.style.display = 'block';
+    })
+})
+
+const stars = document.getElementsByClassName("hov");
+Array.from(stars).forEach((star)=> {
+    star.addEventListener('click', () =>{
+        const element = star.firstChild;
+        (element.style.color == "goldenrod")? (element.style.color= "white"): (element.style.color="goldenrod");
     })
 })
