a9ea4da66b9096aa89d1bdf26daff22e1c448e56
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-07-10 16:49:14 +0500

    Update scripts.js

--- project-list/scripts.js
+++ project-list/scripts.js
@@ -1,24 +1,35 @@
 const hideButtons = document.getElementsByClassName("hid");
 Array.from(hideButtons).forEach((button) => {
-    button.addEventListener('click',() => {
+    button.addEventListener('click', () => {
         const element = button.parentNode;
         element.style.display = 'none';
-    } )
+    })
 })
 
 const unhideButtons = document.getElementById("unhide");
 unhideButtons.addEventListener("click", () => {
-    Array.from(hideButtons).forEach((button) =>{
+    Array.from(hideButtons).forEach((button) => {
         const element = button.parentNode;
         element.style.display = 'block';
     })
 })
 
 const stars = document.getElementsByClassName("hov");
-Array.from(stars).forEach((star)=> {
-    star.addEventListener('click', () =>{
+Array.from(stars).forEach((star) => {
+    star.addEventListener('click', () => {
         const element = star.firstChild;
-        (element.style.color == "goldenrod")? (element.style.color= "white"): (element.style.color="goldenrod");
+        (element.style.color == "goldenrod") ? (element.style.color = "white") : (element.style.color = "goldenrod");
         star.classList.remove("hov");
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Check login status
+    var isLoggedIn = sessionStorage.getItem("isLoggedIn");
+
+    if (!isLoggedIn) {
+        // Redirect to login page
+        window.location.href = "login.html";
+    } 
+});
+
