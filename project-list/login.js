cdf9099f0d05b2476716475b2ae388e7abe565f2
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-07-10 16:28:53 +0500

    Add files via upload

---
+++ project-list/login.js
@@ -0,0 +1,20 @@
+var loginForm = document.getElementById("login-form");
+var loginError = document.getElementById("login-error");
+
+loginForm.addEventListener("submit", (event)  =>{
+    event.preventDefault();
+
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // Static credentials for authentication
+    var validUsername = "admin@gmail.com";
+    var validPassword = "password";
+
+    if (username === validUsername && password === validPassword) {
+        // Redirect to the project list page
+        window.location.href = "index.html";
+    } else {
+        loginError.textContent = "Invalid username or password.";
+    }
+});
