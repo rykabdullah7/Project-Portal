0fbff3a0b4b12da37c7f83162ed3bd69c1721182
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-07-13 12:09:08 +0500

    Update scripts.js

--- project-list/scripts.js
+++ project-list/scripts.js
@@ -1,8 +1,11 @@
 const hideButtons = document.getElementsByClassName("hid");
 Array.from(hideButtons).forEach((button) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', () => {  
         const element = button.parentNode;
-        element.style.display = 'none';
+        const starColor = element.querySelector(".hov > span").style.color;
+        if (starColor !== "rgb(218, 165, 32)") {
+        element.style.display = "none";
+    }
     })
 })
 
@@ -18,18 +21,16 @@ const stars = document.getElementsByClassName("hov");
 Array.from(stars).forEach((star) => {
     star.addEventListener('click', () => {
         const element = star.firstChild;
-        (element.style.color == "rgb(218, 165, 32)") ? (element.style.color = "rgb(255,255,255)") : (element.style.color = "rgb(218, 165, 32)");
-        star.classList.remove("hov");
+        if(element.style.color !== "rgb(218, 165, 32)"){
+            element.style.color = "rgb(218, 165, 32)";
+            star.classList.remove("hov");
+        }
+        else{
+            element.style.color = "rgb(255, 255, 255)";
+            star.classList.add("hov");
+        }
+        
     })
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Check login status
-    var isLoggedIn = sessionStorage.getItem("isLoggedIn");
-
-    if (!isLoggedIn) {
-        // Redirect to login page
-        window.location.href = "login.html";
-    } 
-});
 
