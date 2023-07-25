5b9b5fced8b1b6ff81f70a5d88158f3e0bb1fd91
Muhammad Abdullah Khan Abbasi <75038215+rykabdullah7@users.noreply.github.com>
2023-07-25 18:42:43 +0500

    Update login.js

--- project-list/login.js
+++ project-list/login.js
@@ -1,57 +1,85 @@
-var loginForm = document.getElementById("login-form");
-var loginError = document.getElementById("login-error");
 
-loginForm.addEventListener("submit", (event)  =>{
-    event.preventDefault();
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-    var username = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    // Static credentials for authentication
-    var validUsername = "admin@gmail.com";
-    var validPassword = "password";
-
-    if (username === validUsername && password === validPassword) {
-        sessionStorage.setItem("isLoggedIn","true")
-        // Redirect to the project list page
-        window.location.href = "index.html";
-    } else {
-        loginError.textContent = "Invalid username or password.";
-    }
-});
-var loginForm = document.getElementById("login-form");
-var loginError = document.getElementById("login-error");
-
-loginForm.addEventListener("submit", (event)  =>{
-    event.preventDefault();
-
-    var username = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    // Static credentials for authentication
-    var validUsername = "admin@gmail.com";
-    var validPassword = "password";
-
-    if (username === validUsername && password === validPassword) {
-        // Generate a secure token
-        var accessToken = generateToken();
-        sessionStorage.setItem("accessToken", accessToken);
-
-        // Redirect to the project list page
-        window.location.href = "index.html";
-    } else {
-        loginError.textContent = "Invalid username or password.";
-    }
-});
-
-function generateToken() {
-  // This function can be replaced with a secure token generation mechanism
-  // For this example, a simple random string is generated.
-  var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  var token = "";
-  for (var i = 0; i < 20; i++) {
-      token += chars[Math.floor(Math.random() * chars.length)];
+    this.state = {
+      email: '',
+      password: '',
+      loginError: '',
+    };
   }
-  return token;
+
+  handleLoginSubmit = (event) => {
+    event.preventDefault();
+
+    // Static credentials 
+    const validUsername = 'admin@gmail.com';
+    const validPassword = 'password';
+
+    const { email, password } = this.state;
+
+    if (email === validUsername && password === validPassword) {
+    
+      const accessToken = this.generateToken();
+      sessionStorage.setItem('accessToken', accessToken);
+
+      this.props.onLoginSuccess();
+
+      this.props.history.push('/project-list');
+    } else {
+      this.setState({ loginError: 'Invalid username or password.' });
+    }
+  };
+
+  generateToken() {
+    
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let token = '';
+    for (let i = 0; i < 20; i++) {
+      token += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return token;
+  }
+
+  render() {
+    const { email, password, loginError } = this.state;
+
+    return (
+      <form className="mx-auto mt-5"  style={{ width: 400 }}>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => this.setState({ password: e.target.value })}
+          />
+        </div>
+        {loginError && <p className="error-message">{loginError}</p>}
+        <button type="submit" className="btn btn-primary" onClick={this.handleLoginSubmit}>
+          Login
+        </button>
+      </form>
+    );
+  }
 }
+
 
